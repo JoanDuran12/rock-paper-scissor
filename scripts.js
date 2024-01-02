fa6d9fa60f367903d021628424fe0a4accc5081c
JoanDuran12 <joanmanuel808@gmail.com>
2024-01-02 15:27:25 -0500

    Added win/lose/tie game functionality and Display who win each round

--- scripts.js
+++ scripts.js
@@ -1,4 +1,7 @@
 
+let playerScore = 0
+let computerScore = 0
+
 function getComputerChoice()
 {
     const choices = ["ROCK", "PAPER", "SCISSORS"]
@@ -7,46 +10,57 @@ function getComputerChoice()
 
 function playRound(playerSelection, computerSelection)
 {   
-    // Wins
+    // Win Round
     if((playerSelection == "ROCK" && computerSelection == "SCISSORS")||
        (playerSelection == "PAPER" && computerSelection == "ROCK") ||
        (playerSelection == "SCISSORS" && computerSelection == "PAPER"))
     {
+        playerScore++
+        console.log(`You Win! Player Chose ${playerSelection} beats Computer chose ${computerSelection}`)
         return 1        
     }
-    // Ties
+    // Round Tie
     else if(playerSelection == computerSelection)
     {
+        console.log(`It's a Tie, you both picked ${computerSelection}`)
         return 0
     }
     
-    // Loses
+    // Lose Round
+    computerScore++
+    console.log( `You Lose! Computer chose ${computerSelection} beats Player chose ${playerSelection}`)
     return -1
 }
 
 function game()
 {
+    // Play until one of the player gets 5 points
     for(let i = 0; i < 5; i++)
     {
         let computerSelection = getComputerChoice()
         let playerSelection = prompt("Enter your Choice - Rock, Paper, Scissor")
         playerSelection = playerSelection.toUpperCase()
-        let result = playRound(playerSelection, computerSelection)
 
-        // wins
-        if(result == 1){
-            alert(`You Win! Player Chose ${playerSelection} beats Computer chose ${computerSelection}`)
-        }
-        //Tie
-        else if(result == 0){
-            alert(`It's a Tie, you both picked ${computerSelection}`)
+        let result = playRound(playerSelection, computerSelection)
+        if(result == 0) {
             i--;
-        }   
-        // Lose
-        else{
-            alert( `You Lose! Computer chose ${computerSelection} beats Player chose ${playerSelection}`)
         }
     }
+
+    // Win Game
+    if(playerScore > computerScore)
+    {
+       return `You Won the game, Player: ${playerScore} vs Computer: ${computerScore}`
+    }
+    // Tie Game
+    else if(playerScore == computerScore){
+        return ` It's a Tie,  Player: ${playerScore} vs Computer: ${computerScore}`
+    }
+
+    // Lose Game
+    return `You Lose the game, Computer: ${computerScore} vs Player: ${playerScore}`
+
+
 }
 
 console.log(game())
