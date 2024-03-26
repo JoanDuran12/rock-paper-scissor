493059050081434befeebdd4b1522a8bc90a5c80
Joan Duran <joanmanuel808@gmail.com>
2024-03-26 15:48:57 -0400

    Added endGame function

--- scripts.js
+++ scripts.js
@@ -1,9 +1,6 @@
 
-
 const playerChoices = document.querySelectorAll("#choice");
-const gameResult = document.getElementById("gameResults");
-const currentRound = document.createElement("p")
-gameResult.appendChild(currentRound);
+const currentRound = document.getElementById("currentRound")
 
 // Initialize Players Score 
 let playerScore = 0;
@@ -12,7 +9,8 @@ let computerScore = 0;
 // Add a listener to every player options and run a round
 playerChoices.forEach((choices) => {
     choices.addEventListener("click", () => {
-    playRound(choices.textContent);
+    console.log(choices)
+    playRound(choices.lastElementChild.textContent);
     })    
 })
 
@@ -25,18 +23,29 @@ function getComputerChoice()
 
 // Update Game Score
 function updateScore() {
-    document.getElementById("score").textContent = `Player: ${playerScore} | Computer: ${computerScore}`;
+    document.getElementById("playerScore").textContent = `Player: ${playerScore}`;
+    document.getElementById("computerScore").textContent = `Computer: ${computerScore}`;
 }
 
 // End Game after either the player or computer get 5 points
 function endGame() {
-    if(playerScore >= 5)
-    {
+
+    //Check for whoever have 5 points to end the game
+    if(playerScore === 5)    {
         currentRound.textContent = "Player WIN - GAME OVER";
     }
-    else if (computerScore >= 5) {
+    else if (computerScore === 5) {
         currentRound.textContent = "Computer WIN - GAME OVER";
     }
+
+    //Disable Player Selection
+    if(playerScore === 5 || computerScore === 5)
+    {
+        playerChoices.forEach((choices) =>{
+            choices.setAttribute("disabled", "true")
+        })
+    }
+
 }
 
 // Play a round and return the result
@@ -48,7 +57,7 @@ function playRound(playerSelection)
     if((playerSelection == "Rock" && computerSelection == "Scissors")||
        (playerSelection == "Paper" && computerSelection == "Rock") ||
        (playerSelection == "Scissors" && computerSelection == "Paper")) {
-        currentRound.textContent = `You Win! Player Chose ${playerSelection} beats Computer chose ${computerSelection}`;
+        currentRound.textContent = `You Win! |Player: ${playerSelection}| beats |Computer: ${computerSelection}|`;
         playerScore++    
     }
     // Tie Round
@@ -57,7 +66,7 @@ function playRound(playerSelection)
     }
     // Lose Round
     else {
-        currentRound.textContent = `You Lose! Computer chose ${computerSelection} beats Player chose ${playerSelection}`;
+        currentRound.textContent = `You Lose! |Computer: ${computerSelection}| beats |Player: ${playerSelection}|`;
         computerScore++
     }
     
